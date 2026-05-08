0b2f893a2e6d776dbdfdfc78f149a4af63b18220
MARIUS <mariustsiorimbola@gmail.com>
2026-05-08 19:37:50 +0300

    versionning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss','@netlify/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@netlify/nuxt'],
   css: ['~/assets/css/main.css'],
-
+  nitro: {
+    output: {
+      dir: '.output',
+      publicDir: '.output/public',
+      serverDir: '.output/server',
+    },
+  },
   app: {
     head: {
       style: [{ type: 'text/css', children: 'html { scroll-behavior: smooth; }' }],
